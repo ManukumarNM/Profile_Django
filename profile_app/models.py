699689bbcd05f8db53ed198abf5633a3b2139e0f
ManukumarNM <manunm.jps@gmail.com>
2021-12-07 21:19:38 +0530

    Create contact model

--- profile_app/models.py
+++ profile_app/models.py
@@ -1,9 +1,19 @@
 from django.db import models
+from PIL import Image
 
 # Create your models here.
 class Profile(models.Model):
     name = models.CharField(max_length=15)
-    image = models.ImageField(upload_to="image")
+    image = models.ImageField(upload_to="profile_pics")
+
+    def __str__(self):
+        return self.name 
+
+class Contact(models.Model):
+    name = models.CharField(max_length=200, null=False)
+    email = models.EmailField()
+    subject = models.CharField(max_length=500)
+    message = models.TextField()
 
     def __str__(self):
         return self.name 
