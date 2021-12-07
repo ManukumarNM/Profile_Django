3395ef17beb2a53d55b89f6de67a52bb0295af77
ManukumarNM <manunm.jps@gmail.com>
2021-12-07 21:21:44 +0530

    Initial commit

--- profile_app/admin.py
+++ profile_app/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.contrib.admin.options import ModelAdmin
-from .models import Profile
+from .models import Profile, Contact
 # Register your models here.
 
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['name', 'image']
 admin.site.register(Profile, ProfileAdmin)
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ["name", "email", "subject"]
+admin.site.register(Contact, ContactAdmin)
