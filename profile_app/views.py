2a98a49f1724b3f93307cec96779e247bf4657b4
ManukumarNM <manunm.jps@gmail.com>
2021-12-07 21:20:31 +0530

    initial commit

--- profile_app/views.py
+++ profile_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from .models import Profile
+from .models import Profile, Contact
 # Create your views here.
 
 def index(request):
     profile = Profile.objects.all()
     return render(request, 'index.html', {'profile':profile})
+
+def contactview(request):
+    return render(request, 'contact.html')
