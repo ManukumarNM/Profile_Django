a01940735968661c3a26a224d17deab986353021
ManukumarNM <manunm.jps@gmail.com>
2022-02-04 22:14:45 +0530

    url update

--- profile_app/urls.py
+++ profile_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('contact', views.contact_view, name='contact'),
+    path('', views.index, name='index'), #main page
+    path('contact', views.contact_view, name='contact'), #contact page
     ]
