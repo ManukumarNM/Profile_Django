3c25a38be589be87aa580cb8b52660f7cefe033e
ManukumarNM <manunm.jps@gmail.com>
2022-02-04 21:59:13 +0530

    comment url

--- profile_app/urls.py
+++ profile_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('contact', views.contact_view, name='contact'),
+    # path('contact', views.contact_view, name='contact'),
     ]
